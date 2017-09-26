1d0f2b1d7a24a8589eaa3a81d8b737324ec851bb
hung nguyen <hung nguyen>
2017-09-26 07:46:56 +0700

    xong total price

--- src/components/Main/Shop/Cart/CartView.js
+++ src/components/Main/Shop/Cart/CartView.js
@@ -37,6 +37,8 @@ class CartView extends Component {
             txtName, txtPrice, productImage, numberOfProduct,
             txtShowDetail, showDetailContainer } = styles;
             const { cartArray} = this.props;
+            const arrTotal = cartArray.map(e => e.product.price*e.quantity);
+            const total = arrTotal.length ? arrTotal.reduce((a, b) => a + b) :0;
         return (
             <View style={wrapper}>
 
@@ -76,7 +78,7 @@ class CartView extends Component {
                   )}
                 />
                 <TouchableOpacity style={checkoutButton}>
-                    <Text style={checkoutTitle}>TOTAL {1000}$ CHECKOUT NOW</Text>
+                    <Text style={checkoutTitle}>TOTAL {total}$ CHECKOUT NOW</Text>
                 </TouchableOpacity>
             </View>
         );
